d809f0eabb6f3a99495eb45061e4d65cb0504bd0
Adrien Holvoet <adrien.holvoet@outlook.com>
2024-06-21 17:04:34 +0200

    pass all nested tests

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,12 @@
-import { metadataManager } from "./../managers/index";
-import { JSONObject } from "../json-types";
-import { Permission } from "../permissionModule/type";
 import {
   isPermissionReadable,
   isPermissionWritable,
 } from "../permissionModule/checkPermissionUtil";
+import { PermissionMetadataUtil } from "../permissionModule/metadataUtil";
+import { Permission } from "../permissionModule/type";
+import { JSONObject } from "./../json-types";
 import { IStore } from "./interface";
 import { StoreResult, StoreValue } from "./type";
-import { PermissionMetadataUtil } from "../permissionModule/metadataUtil";
 
 export class Store implements IStore {
   defaultPolicy: Permission = "rw";
@@ -18,24 +17,25 @@ export class Store implements IStore {
     this.currentObject = this;
     this.currentStore = this;
     const nestedKeys: string[] = path.split(":");
-    if (
-      nestedKeys.every((key, index) => {
-        const isNotLastKey = index < nestedKeys.length - 1;
-        if (isNotLastKey && this.currentObject[key] instanceof Store) {
-          this.currentStore = this.currentObject[key];
-        }
-
-        const isAllowed = this.allowedToRead(key);
-        if (isNotLastKey) {
-          this.currentObject = this.currentObject[key];
-        }
-        return isAllowed;
-      })
-    ) {
+    if (this.allKeysHavePermissionsToRead(nestedKeys)) {
       return this.get(path);
     } else {
       throw new Error("Permission denied");
     }
+  }
+
+  private allKeysHavePermissionsToRead(nestedKeys: string[]) {
+    return nestedKeys.every((key, index) => {
+      const isNotLastKey = index < nestedKeys.length - 1;
+      if (isNotLastKey && this.currentObject[key] instanceof Store) {
+        this.currentStore = this.currentObject[key];
+      }
+      const isAllowed = this.allowedToRead(key);
+      if (isNotLastKey) {
+        this.currentObject = this.currentObject[key];
+      }
+      return isAllowed;
+    });
   }
 
   public allowedToRead(key: string): boolean {
@@ -53,26 +53,28 @@ export class Store implements IStore {
     this.currentObject = this;
     this.currentStore = this;
     const nestedKeys: string[] = path.split(":");
-    if (
-      nestedKeys.every((key, index) => {
-        const isNotLastKey = index < nestedKeys.length - 1;
-
-        if (isNotLastKey && this.currentObject[key] instanceof Store) {
-          this.currentStore = this.currentObject[key];
-        }
-
-        const isAllowed = this.allowedToWrite(key);
-        if (isNotLastKey) {
-          this.currentObject = this.currentObject[key];
-        }
-        return isAllowed;
-      })
-    ) {
+    if (this.allKeysHavePermissionsToWrite(nestedKeys)) {
       this.set(path, value);
       return value;
     } else {
       throw new Error("Permission denied");
     }
+  }
+
+  private allKeysHavePermissionsToWrite(nestedKeys: string[]) {
+    return nestedKeys.every((key, index) => {
+      const isNotLastKey = index < nestedKeys.length - 1;
+
+      if (isNotLastKey && this.currentObject[key] instanceof Store) {
+        this.currentStore = this.currentObject[key];
+      }
+
+      const isAllowed = this.allowedToWrite(key);
+      if (isNotLastKey) {
+        this.currentObject = this.currentObject[key];
+      }
+      return isAllowed;
+    });
   }
 
   public allowedToWrite(key: string): boolean {
@@ -97,39 +99,48 @@ export class Store implements IStore {
       }
       this.currentObject = this.currentObject[key];
     }
-
     return this.currentObject;
-  }
-
-  private getProperty(key: string): StoreResult {
-    const descriptor = Object.getOwnPropertyDescriptor(this, key);
-
-    return descriptor?.value;
   }
 
   private set(path: string, value: StoreValue): void {
     const nestedKeys: string[] = path.split(":");
     this.currentObject = this;
 
+    const assignValue = (obj: any, key: string, val: any) => {
+      if (typeof val === "object" && val !== null) {
+        obj[key] = new Store();
+        for (const subKey in val) {
+          if (val.hasOwnProperty(subKey)) {
+            assignValue(obj[key], subKey, val[subKey]);
+          }
+        }
+      } else {
+        obj[key] = val;
+      }
+    };
+
     nestedKeys.forEach((key, index) => {
       const isLastKey = index === nestedKeys.length - 1;
       if (isLastKey) {
-        Object.defineProperty(this.currentObject, key, {
-          value: value,
-        });
+        assignValue(this.currentObject, key, value);
       } else {
-        if (!this.currentObject[key]) {
-          Object.defineProperty(this.currentObject, key, {
-            value: {},
-          });
+        if (
+          !this.currentObject[key] ||
+          !(this.currentObject[key] instanceof Store)
+        ) {
+          this.currentObject[key] = new Store();
         }
         this.currentObject = this.currentObject[key];
       }
     });
   }
 
-  writeEntries(entries: JSONObject): void {
-    throw new Error("Method not implemented.");
+  writeEntries(jsonObject: JSONObject): void {
+    const entries = Object.entries(jsonObject);
+
+    for (const [key, value] of entries) {
+      this.write(key, value);
+    }
   }
 
   entries(): JSONObject {
